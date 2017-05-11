698ae000db042fb60044132f5fe9608c64cdeef0
tacobeard <thedaolson@gmail.com>
2017-05-11 18:23:49 -0500

    Add files via upload

---
+++ create_database_csdb.sql
@@ -0,0 +1,34 @@
+drop database if exists csdb;
+
+create database csdb;
+
+use csdb;
+
+create table tickets (
+id int not null auto_increment,
+ticket_id bigint,
+player_id bigint,
+platform tinytext,
+ticket_created datetime,
+ticket_solved datetime,
+slot_name tinytext,
+ticket_tags tinytext,
+
+primary key (id),
+index (player_id),
+index (ticket_id)
+);
+
+create table comments (
+id int not null auto_increment,
+ticket_id bigint,
+comment_text	text,
+comment_author tinytext,
+comment_datetime date,
+
+primary key (id),
+index (ticket_id),
+
+foreign key (ticket_id)
+references tickets(ticket_id)
+);
