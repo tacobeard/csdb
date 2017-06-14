4e3c959fa9c56d7dac19cb8083594900ab3da9c5
tacobeard <tacobeard@users.noreply.github.com>
2017-06-14 14:55:41 -0500

    Add files via upload

--- insert_csdb.py
+++ insert_csdb.py
@@ -1,7 +1,11 @@
 import ticket_data, pymysql
 
 # Open database connection
-db = pymysql.connect("localhost","testuser","test123","csdb" )
+db = pymysql.connect(host='localhost',
+                       user='testuser',
+                       passwd='test123',
+                       db='csdb',
+                       charset='utf8')
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
@@ -15,9 +19,27 @@ try:
    cursor.execute(sql)
    # Commit your changes in the database
    db.commit()
-except:
-   # Rollback in case there is any error
-   db.rollback()
-
+   print('Database updated')
+except pymysql.ProgrammingError as p:
+   db.rollback()   
+   print ("Caught a Programming Error:")
+   print (p)
+except pymysql.DataError as d:
+   db.rollback() 
+   print ("Caught a Data Error:")
+   print (d)
+except pymysql.IntegrityError as i:
+   db.rollback() 
+   print ("Caught an Integrity Error:")
+   print (i)
+except pymysql.NotSupportedError as n:
+   db.rollback() 
+   print ("Caught a NotSupported Error:")
+   print (n)
+except pymysql.OperationalError as o:
+   db.rollback() 
+   print ("Caught an Operational Error:")
+   print (o)
+    
 # disconnect from server
 db.close()
